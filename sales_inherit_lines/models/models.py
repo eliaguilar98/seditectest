7764944d7e5a670b5080c1065ecd43a70afca8f9
Elizabeth <elizabeth.enriquez@seditec.mx>
2025-01-31 18:36:36 +0000

    Actualizacion nombre

--- sales_inherit_lines/models/models.py
+++ sales_inherit_lines/models/models.py
@@ -4,8 +4,8 @@ from odoo import models, fields, api
 
 
 class sales_inherit_lines(models.Model):
-     _name = 'sales_inherit_lines.sales_inherit_lines'
-     _description = 'sales_inherit_lines.sales_inherit_lines'
+     _name = 'sales_inherit_lines.inherit_lines'
+     _description = 'sales_inherit_lines.inherit_lines'
      _inherit = "sale.order.line"
 
      margin_percent = fields.Float(
