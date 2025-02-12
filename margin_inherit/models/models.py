f75d096f1c8aeefc232cf66b92ec9f7b1afbcf5b
Elizabeth <elizabeth.enriquez@seditec.mx>
2025-02-12 23:39:44 +0000

    Actualizacion de metodo

--- margin_inherit/models/models.py
+++ margin_inherit/models/models.py
@@ -16,10 +16,15 @@ class margin_inherit(models.Model):
 
     purchase_price = fields.Float()
 
-    @api.depends('price_unit','purchase_price','margin_percent')
+    margin = fields.Flotat(
+        
+    )
+
+    @api.depends('price_unit','purchase_price','margin_percent','margin','price_subtotal','product_uom_qty')
     def _compute_price_unit(self):
         for line in self:
-            line.price_unit = line.purchase_price + (line.purchase_price * line.margin_percent)
+            line.price_unit = line.purchase_price + (line.purchase_price * line.margin_percent
+            line.margin = (line.price_unit * line.product_uom_qty) - (line.purchase_price * line.product_uom_qty)
 
     @api.depends()
     def _compute_margin(self):
