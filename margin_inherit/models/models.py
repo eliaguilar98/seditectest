3cac8cdb69e0e7776db1074bb827942b1b345c75
Elizabeth <elizabeth.enriquez@seditec.mx>
2025-02-14 22:15:26 +0000

    Modificaciones

--- margin_inherit/models/models.py
+++ margin_inherit/models/models.py
@@ -26,10 +26,10 @@ class margin_inherit(models.Model):
             line.price_unit = line.purchase_price + (line.purchase_price * line.margin_percent)
             line.margin = (line.price_unit * line.product_uom_qty) - (line.purchase_price * line.product_uom_qty)
 
-    #--@api.depends()
-    #--def _compute_margin(self):
-       # for line in self:
-        #    pass
+    @api.depends()
+    def _compute_margin(self):
+       for line in self:
+            pass
 
 
 
