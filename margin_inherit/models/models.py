9422bb8a1edc60638772725246bc870598b9ef4b
Elizabeth <elizabeth.enriquez@seditec.mx>
2025-02-27 16:30:46 +0000

    Models

--- margin_inherit/models/models.py
+++ margin_inherit/models/models.py
@@ -8,34 +8,34 @@ class margin_inherit(models.Model):
 
     margin_percent = fields.Float(
         readonly = False,
-        compute = '_compute_margin',
-        precompute = False
+        #compute = '_compute_margin',
+        #precompute = False
     )
 
-    price_unit = fields.Float(
-        compute = '_compute_price_unit'
-    )
+   # price_unit = fields.Float(
+    #    compute = '_compute_price_unit'
+    #)
 
-    purchase_price = fields.Float()
+  #  purchase_price = fields.Float()
 
-    margin = fields.Float(
-        compute = '_compute_price_unit'
-    )
+   # margin = fields.Float(
+    #    compute = '_compute_price_unit'
+    #)
 
-    @api.depends('price_unit','purchase_price','margin_percent','margin','price_subtotal','product_uom_qty')
-    def _compute_price_unit(self):
-        for line in self:
-            line.price_unit = line.purchase_price + (line.purchase_price * line.margin_percent)
+    #@api.depends('price_unit','purchase_price','margin_percent','margin','price_subtotal','product_uom_qty')
+ #   def _compute_price_unit(self):
+  #      for line in self:
+   #         line.price_unit = line.purchase_price + (line.purchase_price * line.margin_percent)
             #line.margin = (line.price_unit * line.product_uom_qty) - (line.purchase_price * line.product_uom_qty)
-            line.margin = line.margin_percent * line.price_subtotal
-            print(line.margin)
+    #        line.margin = line.margin_percent * line.price_subtotal
+           
             
 
-    @api.depends('price_subtotal','margin_percent','margin')
-    def _compute_margin(self):
-       for line in self:
-            line.margin = line.margin_percent * line.price_subtotal
-            line.margin_percent = line.margin_percent
+    #@api.depends('price_subtotal','margin_percent','margin')
+    #def _compute_margin(self):
+     #  for line in self:
+      #      line.margin = line.margin_percent * line.price_subtotal
+       #     line.margin_percent = line.margin_percent
 
 
 
