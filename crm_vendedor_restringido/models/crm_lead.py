61a772a2c4224d82e9923192f3db24b9b3ba191d
Elizabeth <elizabeth.enriquez@seditec.mx>
2025-05-16 18:50:56 +0000

    [Modificaciones v4]

---
+++ crm_vendedor_restringido/models/crm_lead.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api 
+
+class crm_Lead(models.Model):
+    _inherit = 'crm_lead'
+
+    can_edit_user_id = fields.Boolean(compute='_compute_can_edit_user_id', store=False)
+
+    @api.depends()
+    def _compute_can_edit_user_id(self):
+        for record in self:
+            user = self.env.user
+            record.can_edit_user_id = user.has_group('sales_team.group_sale_manager')
