dd58dfb0def592e23148f6b66e2ff72875cdf27b
elizabeth <elizabeth.enriquez@seditec.mx>
2025-05-19 16:10:46 +0000

    Modificaciones v18

--- crm_vendedor_restringido/models/res_partner.py
+++ crm_vendedor_restringido/models/res_partner.py
@@ -5,10 +5,17 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    can_edit_user_id = fields.Boolean(compute='_compute_can_edit_user_id', store=False)
+     @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'user_id' in vals and not self.env.user.has_group('sales_team.group_sale_manager'):
+                raise UserError(_("Solo un gerente de ventas puede asignar un vendedor."))
+        return super().create(vals)
 
-    @api.depends()
-    def _compute_can_edit_user_id(self):
-        for record in self:
-            user = self.env.user
-            record.can_edit_user_id = user.has_group('sales_team.group_sale_manager')
+    def write(self, vals):
+        if 'user_id' in vals:
+            for lead in self:
+                if not self.env.user.has_group('sales_team.group_sale_manager'):
+                    raise ValidationError(_("Solo un gerente de ventas puede cambiar el vendedor asignado."))
+
+        return super().write(vals)
