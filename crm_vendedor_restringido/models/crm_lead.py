bf62c9dc9f90bd56330874bb4e495ce8b3eea3d2
elizabeth <elizabeth.enriquez@seditec>
2025-05-19 15:23:26 +0000

    [Modificaciones v17]

--- crm_vendedor_restringido/models/crm_lead.py
+++ crm_vendedor_restringido/models/crm_lead.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api 
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
 
 class crm_Lead(models.Model):
     _inherit = 'crm.lead'
 
-    @api.model
-    def create(self, vals):
-        if 'user_id' in vals and not self.env.user.has_group('sales_team.group_sale_manager'):
-            raise UserError(_("Solo un gerente de ventas puede asignar un vendedor."))
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'user_id' in vals and not self.env.user.has_group('sales_team.group_sale_manager'):
+                raise UserError(_("Solo un gerente de ventas puede asignar un vendedor."))
         return super().create(vals)
 
     def write(self, vals):
-        if 'user_id' in vals and not self.env.user.has_group('sales_team.group_sale_manager'):
-            raise ValidationError(_("Solo un gerente de ventas puede cambiar el vendedor asignado."))
+        if 'user_id' in vals:
+            for lead in self:
+                if not self.env.user.has_group('sales_team.group_sale_manager'):
+                    raise ValidationError(_("Solo un gerente de ventas puede cambiar el vendedor asignado."))
 
         return super().write(vals)
