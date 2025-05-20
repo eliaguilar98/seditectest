311c463d0c8e439e75a98ea8c6a5ea5591a3fa48
elizabeth <elizabeth.enriquez@seditec.mx>
2025-05-20 23:24:57 +0000

    Correcciones

--- crm_vendedor_restringido/models/res_partner.py
+++ crm_vendedor_restringido/models/res_partner.py
@@ -12,7 +12,7 @@ class ResPartner(models.Model):
         for vals in vals_list:
             if 'user_id' in vals and not self.env.user.has_group('sales_team.group_sale_manager'):
                 raise UserError(_("Solo un gerente de ventas puede asignar un vendedor."))
-        return super().create(vals)
+        return super().create(vals_list)
 
     def write(self, vals):
         if 'user_id' in vals:
