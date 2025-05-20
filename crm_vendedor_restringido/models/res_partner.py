18816a50cc46d1c9851caa81490519058e37a8d8
elizabeth <elizabeth.enriquez@seditec.mx>
2025-05-20 23:32:22 +0000

    Permitir crear

--- crm_vendedor_restringido/models/res_partner.py
+++ crm_vendedor_restringido/models/res_partner.py
@@ -7,13 +7,6 @@ from datetime import datetime
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if 'user_id' in vals and not self.env.user.has_group('sales_team.group_sale_manager'):
-                raise UserError(_("Solo un gerente de ventas puede asignar un vendedor."))
-        return super().create(vals_list)
-
     def write(self, vals):
         if 'user_id' in vals:
             for lead in self:
