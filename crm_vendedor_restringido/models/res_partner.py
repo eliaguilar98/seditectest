edc5a6282e8d6a7dd67a6438c05c09f9980c4d0b
Elizabeth <elizabeth.enriquez@seditec.mx>
2025-05-20 22:22:32 +0000

    Modificaciones v26

--- crm_vendedor_restringido/models/res_partner.py
+++ crm_vendedor_restringido/models/res_partner.py
@@ -15,10 +15,10 @@ class ResPartner(models.Model):
         return super().create(vals)
 
     def write(self, vals):
-        if 'user_id' in vals:
+       if 'user_id' in vals:
             for lead in self:
                 if not self.env.user.has_group('sales_team.group_sale_manager'):
-                    raise ValidationError(_("🔒 Solo un gerente de ventas tiene permiso para cambiar el vendedor asignado.Si necesitas hacer este cambio, por favor contacta a tu gerente."))
+                    raise ValidationError(_("🔒 Solo un gerente de ventas tiene permiso para cambiar el vendedor asignado. Si necesitas hacer este cambio, por favor contacta a tu gerente."))
 
                 # Obtener datos del vendedor anterior y nuevo 
                 old_user = lead.user_id
@@ -43,17 +43,4 @@ class ResPartner(models.Model):
                         subtype_xmlid="mail.mt_note",
                     )
 
-        return super().write(vals)# Obtener datos del vendedor anterior y nuevo 
-                old_user = lead.user_id
-                new_user = self.env['res.users'].browse(vals['user_id'])
-
-                if old_user != new_user: 
-                    lead.message_post(
-                        body =_("El vendedor asignado fu cambiado de <b>%s</b> a <b>%s</b> por <i>%s</i>.") % (
-                            old_user.name or _("Sin asignar"),
-                            new_user.name or _("Sin asignar"),
-                            self.env.user.name,
-                        ),
-                        subtype_xmlid="mail.mt_note",
-                    )
         return super().write(vals)
