d4de26d94e27ed281353f5a9964b9e1ca9d199c9
elizabeth <elizabeth.enriquez@seditec.mx>
2025-05-26 16:46:35 +0000

    Upgrade v18

--- margin_inherit/__manifest__.py
+++ margin_inherit/__manifest__.py
@@ -18,7 +18,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','sale_management'],
+    'depends': ['base','sale','sale_management','sale_margin'],
 
     # always loaded
     'data': [
