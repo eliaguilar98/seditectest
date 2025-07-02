48d67ed3f7e3e7bb492291cd399d3d4b7094c9e2
elizabeth <elizabeth.enriquez@seditec.mx>
2025-07-02 17:51:41 +0000

    Añadir Licencia

--- margin_inherit/__manifest__.py
+++ margin_inherit/__manifest__.py
@@ -15,7 +15,8 @@ Long description of module's purpose
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.1',
+    'license': 'LGPL-30',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale','sale_management','sale_margin'],
