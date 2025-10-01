d5f7f73798717e7a4799f0763c6c99cb4880dab4
Eli <elizabeth.enriquez@seditec.mx>
2025-10-01 22:39:58 +0000

    Correccion de Margen Global

--- margin_inherit/models/models.py
+++ margin_inherit/models/models.py
@@ -10,15 +10,19 @@ class margin_inherit(models.Model):
         readonly = False,
     )
 
-    price_unit = fields.Float(
-        compute = '_compute_price_unit'
-    )
-
-    @api.depends('margin_percent', 'purchase_price')
-    def _compute_price_unit(self):
+    @api.onchange('margin_percent', 'purchase_price')
+    def _onchange_margin_percent(self):
         for line in self:
-            if line.purchase_price != 0 and line.margin_percent != 0:
+            "Recalcula el precio unitario cuando cambia el margen o el costo"
+            if line.purchase_price and line.margin_percent:
                 line.price_unit = line.purchase_price / (1 - line.margin_percent)
+
+    @api.onchange('price_unit','purchase_price')
+    def _onchange_price_unit(self):
+        "Recalcula el margen cuando cambia el precio unitario"
+        for line in self:
+            if line.purchase_price and line.price_unit:
+                line.margin_percent = (1-(line.purchase_price / line.price_unit))
 
 
 
