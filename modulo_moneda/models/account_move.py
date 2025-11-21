c822a2d7886184f57cb918029e3927b7f1ce367f
Elizabeth <elizabeth.enriquez@seditec.mx>
2025-11-21 23:06:24 +0000

    Testear tipo de cambio

--- modulo_moneda/models/account_move.py
+++ modulo_moneda/models/account_move.py
@@ -8,35 +8,39 @@ class AccountMove(models.Model):
 
     @api.onchange('currency_id')
     def _onchange_currency_id_convert_lines(self):
-        for move in self: 
-            if move.move_type not in ('out_invoice','in_invoice'):
-                continue # Solo aplica a facturas
+        print("\n\n### ONCHANGE EJECUTADO ###\n\n")
+        for move in self:
+            print(f"Factura: {move.name}, Moneda nueva: {move.currency_id.name}")
 
-            if not move.currency_id or not move.company_id: 
-                continue
+        #for move in self: 
+         #   if move.move_type not in ('out_invoice','in_invoice'):
+          #      continue # Solo aplica a facturas
 
-            company = move.company_id
-            factura_currency = move.currency_id
-            company_currency = company.currency_id
+           # if not move.currency_id or not move.company_id: 
+            #    continue
 
-            # Obtener tipo de cambio vigente
-            rate = factura_currency._get_conversion_rate(
-                company_currency,
-                factura_currency,
-                company,
-                move.invoice_date or fields.Date.context_today(move)
-            )
+           # company = move.company_id
+            #factura_currency = move.currency_id
+            #company_currency = company.currency_id
 
-            if not rate: 
-                raise UserError("No existe un tipo de cambio válido para la fecha")
+            ## Obtener tipo de cambio vigente
+            #rate = factura_currency._get_conversion_rate(
+             #   company_currency,
+              #  factura_currency,
+               # company,
+               # move.invoice_date or fields.Date.context_today(move)
+            #)
+
+            #if not rate: 
+             #   raise UserError("No existe un tipo de cambio válido para la fecha")
 
             #Convertimos cada linea
-            for line in move.invoice_line_ids:
-                if line.price_unit:
-                    line.price_unit = company_currency._convert(
-                        line.price_unit,
-                        factura_currency,
-                        company,
-                        move.invoice_date or fields.Date.context_today(move)
-                    )
+            #for line in move.invoice_line_ids:
+             #   if line.price_unit:
+              #      line.price_unit = company_currency._convert(
+               #         line.price_unit,
+                #        factura_currency,
+                 #       company,
+                  #      move.invoice_date or fields.Date.context_today(move)
+                   # )
            
