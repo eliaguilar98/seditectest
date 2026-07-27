2852e38b88255a8a3ffdccd95a60c64c10764095
elizabeth <elizabeth.enriquezseditec.mx>
2026-07-27 07:55:47 +0000

    Actualizar crm v19

--- crm_vendedor_restringido/__manifest__.py
+++ crm_vendedor_restringido/__manifest__.py
@@ -17,7 +17,7 @@ Ideal para asegurar la trazabilidad comercial y evitar reasignaciones no control
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '1.0',
+    'version': '19.0.1.1',
     'license': 'OPL-1',
 
     # any module necessary for this one to work correctly
@@ -34,5 +34,7 @@ Ideal para asegurar la trazabilidad comercial y evitar reasignaciones no control
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': False,
 }
 
