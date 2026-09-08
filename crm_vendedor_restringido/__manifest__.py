2f310859a877f3d40e3608cb9a9b5b214f60704a
elizabeth <elizabeth.enriquez@seditec.mx>
2026-09-08 22:20:28 +0000

    Version18

--- crm_vendedor_restringido/__manifest__.py
+++ crm_vendedor_restringido/__manifest__.py
@@ -17,7 +17,7 @@ Ideal para asegurar la trazabilidad comercial y evitar reasignaciones no control
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '19.0.1.1',
+    'version': '18.0.1.1',
     'license': 'OPL-1',
 
     # any module necessary for this one to work correctly
