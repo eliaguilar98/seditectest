0da31e7cea06dd00456f7fa178884515728da432
elizabeth <elizabeth.enriquez@seditec.mx>
2026-09-25 17:37:32 +0000

    Modificacion de codigo margen

--- margin_inherit/models/models.py
+++ margin_inherit/models/models.py
@@ -7,46 +7,31 @@ class margin_inherit(models.Model):
     _inherit = "sale.order.line"
 
     margin_percent = fields.Float(
+        sting = "Margen (%)",
         readonly = False,
     )
-    price_subtotal = fields.Float(
-        readonly = False,
-    )
+    #price_subtotal = fields.Float(
+     #   readonly = False,
+    #)
 
     @api.onchange('margin_percent', 'purchase_price')
     def _onchange_margin_percent(self):
         "Recalcula el precio unitario cuando cambia el margen o el costo"
-        if not self: 
-            return
-        # Solo aplica en la línea actual (self representa la linea activa en el formulario)
-        line = self[0]
-
-        #Validaciones básicas para evitar división por cero o valores inválidos
-        if line.purchase_price and line.margin_percent < 1:
-            try: 
+        for line in self:
+            # Asegura que el costo exista y el margen sea menor al 100%
+            # (Un margen de 1.0 causaría división por cero
+            if line.purchase_price and line.margin_percent < 1.0: 
                 line.price_unit = line.purchase_price / (1 - line.margin_percent)
-            except ZeroDivisionError:
-                line.price_unit = line.purchase_price
-        else: 
-            #Si no hay margen válido, no recalcula
-            pass
 
     @api.onchange('price_unit','purchase_price')
     def _onchange_price_unit(self):
         "Recalcula el margen cuando cambia el precio unitario"
-        if not self:
-            return
-        # Solo aplica en la línea actual (self representa la linea activa en el formulario)
-        line = self[0]
-
-        # Validaciones básicas para evitar división por cero o valores inválidos
-        if line.purchase_price and line.price_unit < 1:
-            try:
-                line.margin_percent = (1-(line.purchase_price / line.price_unit))
-            except ZeroDivisionError:
-                line.margin_percent = 0
-        else: 
-            pass
+        for line in self:
+            # Previene ZeroDivisionError verificando que price_unit no sea 0
+            if line.purchase_price and line.price_unit:
+                line.margin_percent = 1.0 - (line.purchase_price / line.price_unit)
+            elif not line.price_unit:
+                line.margin_percent = 0.0
 
 
 
